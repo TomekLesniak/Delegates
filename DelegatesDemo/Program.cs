2542962c92b7d8ebac6ee4a4cbb63fdc86a976ba
TomekLesniak <tomasz.lesniak152@gmail.com>
2020-12-15 20:41:43 +0100

    Add project files.

---
+++ DelegatesDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DelegatesLibrary;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new TotalCostCalculator(100);
+            var totalCost = calculator.CalculateTotalCost(price => price * 0.23m, ConsoleNotification);
+
+            Console.WriteLine($"Total cost for $50 after tax for Poland is: ${totalCost}");
+        }
+
+        private static void ConsoleNotification(string message)
+        {
+            Console.WriteLine($"NOTIFICATION: {message}");
+        }
+    }
+}
---
+++ DelegatesLibrary/TotalCostCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DelegatesLibrary
+{
+    public class TotalCostCalculator
+    {
+        private decimal _price;
+
+        public TotalCostCalculator(decimal price)
+        {
+            _price = price;
+        }
+
+        public decimal CalculateTotalCost(Func<decimal, decimal> calculateTax, Action<string> notifyUser)
+        {
+            var taxForGivenCountry = calculateTax(_price);
+            _price += taxForGivenCountry;
+
+            if (IsEligibleForDiscount())
+            {
+                notifyUser("You have received a discount!");
+                ApplyDiscount();
+            }
+
+            return _price;
+        }
+
+        private bool IsEligibleForDiscount()
+        {
+            return _price >= 100.0M;
+        }
+
+        private void ApplyDiscount()
+        {
+            _price -= 10.0M;
+        }
+
+    }
+}
